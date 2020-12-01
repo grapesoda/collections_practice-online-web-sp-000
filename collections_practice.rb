f76cc082961cd8725ed0f5282c5744abf1a47739
Tres Tarwater <trest03@gmail.com>
2020-12-01 20:01:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,17 +14,17 @@ end
 
 ####---------------####
 
-def sort_array_char_count(numbers)
-  numbers.sort do |a, b|
+def sort_array_char_count(array)
+  array.sort do |a, b|
     a.length <=> b.length
   end
 end
   
 ####---------------####
 
-def swap_elements(numbers)
-  numbers[1], numbers[2] = numbers[2], numbers[1]
-  numbers
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
 end
     
 ####---------------####
